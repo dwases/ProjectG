b970306f33876aadef8d823d98fe2334faa4e84a
dwases <94984468+dwases@users.noreply.github.com>
2025-12-27 19:38:55 +0100

    base artifact changes

--- project-g/Scripts/Artifacts/base_artefact.gd
+++ project-g/Scripts/Artifacts/base_artefact.gd
@@ -6,4 +6,5 @@ class_name ArtifactData
 @export var icon: Texture2D
 @export var description: String = ""
 # Tutaj możesz dodać skrypt, który definiuje zachowanie (Logic)
-@export var effect_script: GDScript
+#@export var effect_script: GDScript
+@export var effect_script: BaseArtifactScript
--- project-g/Scripts/Artifacts/base_artifact_script.gd
+++ project-g/Scripts/Artifacts/base_artifact_script.gd
@@ -1,1 +1,9 @@
-extends Node
+class_name BaseArtifactScript extends Resource
+
+var player_stats = preload("uid://dtgvrioaqwptt")
+
+func apply_constant_stat_changes(stats_component : StatsComponent, strength_change : int,
+dexterity_change : int, luck_change : int) -> void:
+	stats_component.modify_stat("strength", strength_change)
+	stats_component.modify_stat("dexterity", dexterity_change)
+	stats_component.modify_stat("luck", luck_change)
